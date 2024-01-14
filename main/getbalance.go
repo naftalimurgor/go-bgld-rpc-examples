c9eb9d0e1135b50ec213269fca7be01836853acb
naftalimurgor <murgornaftali@gmail.com>
2024-01-14 15:45:36 +0300

    add `getbalance` example

---
+++ main/getbalance.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/naftalimurgor/go-bgld"
+	"log"
+)
+
+const (
+	SERVER_HOST        = ""
+	SERVER_PORT        = 8334
+	USER               = "localuser"
+	PASSWD             = "3xaO3o/i-]G4"
+	USESSL             = false
+	WALLET_PASSPHRASE  = "p1"
+	WALLET_PASSPHRASE2 = "p2"
+)
+
+func main() {
+	bc, err := bgld.New(SERVER_HOST, SERVER_PORT, USER, PASSWD, USESSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	balance, err := bc.GetBalance("tests", 0)
+	log.Println(err, balance)
+}
